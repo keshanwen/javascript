283003e1140c0cdd23defa2d62cb6ae3dbd385c5
keshanwen <624953505@qq.com>
2020-08-16 18:31:44 +0800

    js数据类型

---
+++ 数据类型/1简介.js
@@ -0,0 +1,24 @@
+// 数据类型
+
+/**
+ * 1 number
+ * 2 undefine
+ * 3 string
+ * 4 null
+ * 5 booeal
+ * 6 object
+ * 7 symbol
+ *  */ 
+
+
+/**
+ * 检测数据类型的方法
+ * 
+ * 1 typeof
+ * 
+ * 2 instanceOf
+ * 
+ * 3 Object.prototype.tostring
+ * 
+ * 
+ *  */ 
---
+++ 数据类型/2null,undefined,booeal.js
@@ -0,0 +1,30 @@
+/**
+ * null 
+ * 表示 空 的对象，转换为数值时为 0
+ * 
+ * undefined
+ * 
+ * 表示 此处无定义的原始值，转换为数值时为NaN
+ *  */ 
+
+ Number(null)  
+ Number(undefined)
+
+
+/**
+ * 以下的值会转换为false 其他的值都转换为true
+ * 
+ * undefined
+ * 
+ * null
+ * 
+ * false
+ * 
+ * 0
+ * 
+ * NaN
+ * 
+ * 
+ * '' ""空字符串
+ * 
+ *  */ 
